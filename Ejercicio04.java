73adb12e2dbf7ad5dda614732035084b52516a9e
LucianoIvan22 <102632682+LucianoIvan22@users.noreply.github.com>
2022-04-04 17:23:45 -0300

    Update Ejercicio04.java

--- Ejercicio04.java
+++ Ejercicio04.java
@@ -1,6 +1,6 @@
 // Ejercicio04 - obtencion de las diagonales de una matriz
-//Escribir un mÈtodo en Java que de una matriz cuadrada dada, devuelva una matriz con
-//n filas, donde cada una tendr· los datos de una de las diagonales 
+//Escribir un m√©todo en Java que de una matriz cuadrada dada, devuelva una matriz con
+//n filas, donde cada una tendr√° los datos de una de las diagonales 
 //(de abajo a la izquierda hacia arriba a la derecha)
 
 //Ejemplo:
@@ -20,35 +20,73 @@
 // 4
 
 public class Ejercicio04 {
-	public int[][] obtencionDeDiagonales(int[][] matriz) {
-		int tam = 0, cols = 0, fils = matriz.length - 1, nuevaFila = 0, cantDiagonales = matriz.length * 2 - 1;
-		int[][] nuevaMatriz = new int[cantDiagonales][];
-		while (nuevaFila < cantDiagonales) {
-			nuevaMatriz[nuevaFila] = new int[tam + 1];
-			for (int j = cols, i = fils; j < matriz.length && i < matriz.length; j++, i++) {
-				nuevaMatriz[nuevaFila][j - cols] = matriz[i][j];
+	public int[][] obtenerDiagonalesMatriz(int[][] matriz) {
+
+		// La cantidad de diagonales de una matriz cuadrada es el doble del orden de la
+		// matriz - 1
+		int ordenMatriz = matriz.length;
+		int cantidadDiagonales = ordenMatriz * 2 - 1;
+
+		int[][] matrizResultado = new int[cantidadDiagonales][];
+
+		// Voy a recorrer la matriz resultado por fila, de abajo hacia arriba
+		int indiceX = ordenMatriz - 1;
+		int indiceY = 0;
+		int tam = 1;
+
+		for (int diagonal = 0; diagonal < cantidadDiagonales; diagonal++) {
+			// Arranco con un extremo
+			int auxIndiceX = indiceX;
+			int auxIndiceY = indiceY;
+			matrizResultado[diagonal] = new int[tam];
+			// Incremento indiceX e indiceY +1 para poder recorrer la diagonal hasta que
+			// algun indice supere el rango (ordenMatriz)
+			while (auxIndiceX < ordenMatriz && auxIndiceY < ordenMatriz) {
+
+				matrizResultado[diagonal][auxIndiceY - indiceY] = matriz[auxIndiceX][auxIndiceY];
+				auxIndiceX++;
+				auxIndiceY++;
 			}
-			nuevaFila++;
-			if (nuevaFila < matriz.length) {
-				fils--;
-				tam ++;
-			}
-			else {
-				cols++;
+
+			// Si el indice X llego a 0, no lo puedo decrementar mas. Recorro la matriz por
+			// columnas
+			if (indiceX == 0) {
+				indiceY++;
 				tam--;
+			} else {
+				indiceX--;
+				tam++;
 			}
 		}
-		return nuevaMatriz;
+
+		return matrizResultado;
 	}
 
 	public static void main(String[] args) {
+
+		//int[][] matriz = { { 1, 2, -3 }, { 5, -6, -6 }, { 1, 1, 10 } };
 		int[][] matriz = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-		Ejercicio04 ejercicio04 = new Ejercicio04();
-		int[][] resultado = ejercicio04.obtencionDeDiagonales(matriz);
-		for (int i = 0; i < resultado.length; i++) {
-			for (int j = 0; j < resultado[i].length; j++)
-				System.out.print(resultado[i][j] + "  ");
-			System.out.println();
+
+		Ejercicio04 ejercicioTest = new Ejercicio04();
+
+		System.out.println("Matriz de ejemplo: ");
+		ejercicioTest.mostrarMatriz(matriz);
+
+		int[][] matrizDeDiagonales = ejercicioTest.obtenerDiagonalesMatriz(matriz);
+
+		System.out.println("Matriz de diagonales: ");
+		ejercicioTest.mostrarMatriz(matrizDeDiagonales);
+	}
+
+	public void mostrarMatriz(int[][] matriz) {
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j]);
+				System.out.print("\t");
+			}
+			System.out.print("\n");
 		}
+
 	}
 }
